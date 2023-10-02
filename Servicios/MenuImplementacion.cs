9f743532b13adc934382de19e36d4b85f4a6178e
Eduardo Ballesteros <ballesterospereze@gmail.com>
2023-10-02 19:49:11 +0200

    Menu de seleccion y mensaje bienvenida

--- Servicios/MenuImplementacion.cs
+++ Servicios/MenuImplementacion.cs
@@ -7,11 +7,43 @@ using System.Threading.Tasks;
 namespace ContadorHorasConsola.Servicios
 {
     /// <summary>
-    /// Controlador de horas que implementa a Menu Interfaz
+    /// Controlador de horas que implementa a Menu Interfaz (IMPLEMENTACION DE LA INTERFAZ)
     /// 26092023 - ebp
     /// </summary>
     
-    internal class MenuImplementacion
+    internal class MenuImplementacion : MenuInterfaz 
     {
+
+        public void mensajeBienvenida()
+        {
+            String mensaje = "Controlador de horas de Edu Ballesteros";
+            Console.WriteLine(mensaje);
+
+        }
+
+
+        int MenuInterfaz.mostrarSeleccionarMenu()
+        {
+            int opcionSeleccionadaMenu;
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("0. Cerrar aplicación");
+            Console.WriteLine("1. Crear usuario");
+            Console.WriteLine("2. Registro de horas");
+            Console.WriteLine("3. Cantidad de horas");
+            Console.WriteLine("4. Comparación de horas");
+            Console.WriteLine("5. Historial de juego");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Seleccione una opción:     ");
+
+            //Guardamos la respuesta del usuario(ReadKey - Solo lee un cáracter)
+            //KeyChar (Guarda la primera tecla que pulse el usuario)
+
+            opcionSeleccionadaMenu = Console.ReadKey(true).KeyChar - ('0');
+            return opcionSeleccionadaMenu;
+        }
     }
+
+    
+    
 }
--- Servicios/MenuInterfaz.cs
+++ Servicios/MenuInterfaz.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace ContadorHorasConsola.Servicios
 {
-   /// <summary>
-   /// Interfaz que redacta los distintos métodos de la funcionalidad del menú
-   /// 26092023 - ebp
-   /// </summary>
+    /// <summary>
+    /// Interfaz que redacta los distintos métodos de la funcionalidad del menú
+    /// 26092023 - ebp
+    /// </summary>
     internal interface MenuInterfaz
     {
+
+        /// <summary>
+        /// Método que muestra un mensaje de bienvenida al programa
+        /// 021023 - ebp
+        /// </summary>
+
+
+        public void mensajeBienvenida();
+
+        /// <summary>
+        /// Método que muestra las diferentes opciones del programa
+        /// 26092023 - ebp
+        /// </summary>
+
+        public int mostrarSeleccionarMenu();
     }
 }
